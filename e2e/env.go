1de40f3cd215708bd2c20af1d1fc77caafbe96ad
Walentalien <voliinyk@student.agh.edu.pl>
2025-09-27 23:05:11 +0200

    revised version of env loader

--- e2e/env.go
+++ e2e/env.go
@@ -7,8 +7,9 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// LoadEnv loads the environment from .env in local runs; fails if required vars are not set
 func LoadEnv() error {
-	_ = godotenv.Load() // silently load .env
+	_ = godotenv.Load() // silently load .env; ignore error bc in CI it's expected to fail and env is from repo secrets
 
 	required := []string{"SERVICE_URL"}
 	for _, key := range required {
